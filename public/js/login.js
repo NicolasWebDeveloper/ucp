e1835a2ae22ee5e09914f18975f04cd94bf6c768
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-12-20 20:03:22 +0100

    Added notification

--- public/js/login.js
+++ public/js/login.js
@@ -1,45 +1,60 @@
 const form = document.querySelector('.form');
+const body = document.querySelector('body');
 const nameInput = document.querySelector('.name');
 const passwordInput = document.querySelector('.password');
 const error = document.querySelector('.error');
 const errorContainer = document.querySelector('.container__error');
 const loader = document.querySelector('.container__loader');
+const notificationAudio = new Audio('/audio/notification.mp3');
 
-const showError = (name, password, msg) => {
-  errorContainer.classList.remove('hidden');
+const notify = (type, message) => {
+  notificationAudio.play();
+  const errorEl = document.createElement('div');
+  errorEl.classList.add(type === 'success' ? 'notification__success' : 'notification__error');
+  errorEl.textContent = message;
+  body.appendChild(errorEl);
+  setTimeout(() => {
+    errorEl.parentElement.removeChild(errorEl);
+  }, 5000);
+};
+
+const validationError = (name, password, msg) => {
   if (msg) {
     return (error.textContent = msg);
   }
 
   if (!name && !password) {
-    error.textContent = 'No name and password';
+    notify('error', 'No name and password');
     return nameInput.focus();
   }
 
   if (!name) {
-    error.textContent = 'Please provide a name!';
+    notify('error', 'Please provide a name!');
     return nameInput.focus();
   }
 
   if (!password) {
-    error.textContent = 'Please provide a password!';
+    notify('error', 'Please provide a password!');
     return passwordInput.focus();
   }
 };
 
 const loginUser = async (name, password) => {
   loader.classList.remove('hidden');
+  console.log(JSON.stringify({ name, password }));
   try {
-    const res = await fetch({
+    const res = await fetch('/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
-      }
+      },
+      body: JSON.stringify({ name, password })
     });
     if (!res.ok) throw new Error(res.statusText);
+    notify('success', 'Welcome back!');
   } catch (err) {
     loader.classList.add('hidden');
-    showError(null, null, err.message);
+    notify('error', err.message);
   }
 };
 
@@ -48,7 +63,7 @@ form.addEventListener('submit', e => {
   const name = nameInput.value;
   const password = passwordInput.value;
 
-  if (!name || !password) return showError(name, password);
+  if (!name || !password) return validationError(name, password);
   errorContainer.classList.add('hidden');
 
   loginUser(name, password);
